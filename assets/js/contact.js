acff107b0351ee26a3b1151df5227ffd3e786f83
Igor Ostrovskyi <30428826+ostrik2@users.noreply.github.com>
2022-05-26 13:00:54 +0300

    Navigation to PUG

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,85 +1,85 @@
 $(document).ready(function () {
 
-    (function ($) {
-        "use strict";
+	(function ($) {
+		"use strict";
 
 
-        jQuery.validator.addMethod('answercheck', function (value, element) {
-            return this.optional(element) || /^\bcat\b$/.test(value)
-        }, "type the correct answer -_-");
+		jQuery.validator.addMethod('answercheck', function (value, element) {
+			return this.optional(element) || /^\bcat\b$/.test(value)
+		}, "type the correct answer -_-");
 
-        // validate contactForm form
-        $(function () {
-            $('#contactForm').validate({
-                rules: {
-                    name: {
-                        required: true,
-                        minlength: 2
-                    },
-                    subject: {
-                        required: true,
-                        minlength: 4
-                    },
-                    number: {
-                        required: true,
-                        minlength: 5
-                    },
-                    email: {
-                        required: true,
-                        email: true
-                    },
-                    message: {
-                        required: true,
-                        minlength: 20
-                    }
-                },
-                messages: {
-                    name: {
-                        required: "come on, you have a name, don't you?",
-                        minlength: "your name must consist of at least 2 characters"
-                    },
-                    subject: {
-                        required: "come on, you have a subject, don't you?",
-                        minlength: "your subject must consist of at least 4 characters"
-                    },
-                    number: {
-                        required: "come on, you have a number, don't you?",
-                        minlength: "your Number must consist of at least 5 characters"
-                    },
-                    email: {
-                        required: "no email, no message"
-                    },
-                    message: {
-                        required: "um...yea, you have to write something to send this form.",
-                        minlength: "thats all? really?"
-                    }
-                },
-                submitHandler: function (form) {
-                    $(form).ajaxSubmit({
-                        type: "POST",
-                        data: $(form).serialize(),
-                        url: "contact_process.php",
-                        success: function () {
-                            $('#contactForm :input').attr('disabled', 'disabled');
-                            $('#contactForm').fadeTo("slow", 1, function () {
-                                $(this).find(':input').attr('disabled', 'disabled');
-                                $(this).find('label').css('cursor', 'default');
-                                $('#success').fadeIn()
-                                $('.modal').modal('hide');
-                                $('#success').modal('show');
-                            })
-                        },
-                        error: function () {
-                            $('#contactForm').fadeTo("slow", 1, function () {
-                                $('#error').fadeIn()
-                                $('.modal').modal('hide');
-                                $('#error').modal('show');
-                            })
-                        }
-                    })
-                }
-            })
-        })
+		// validate contactForm form
+		$(function () {
+			$('#contactForm').validate({
+				rules: {
+					name: {
+						required: true,
+						minlength: 2
+					},
+					subject: {
+						required: true,
+						minlength: 4
+					},
+					number: {
+						required: true,
+						minlength: 5
+					},
+					email: {
+						required: true,
+						email: true
+					},
+					message: {
+						required: true,
+						minlength: 20
+					}
+				},
+				messages: {
+					name: {
+						required: "Адже у вас є ім'я, чи не так?",
+						minlength: "Ваше ім'я повинно складатися як мінімум з 2 символів."
+					},
+					subject: {
+						required: "Гаразд, у Вас є тема, чи не так?",
+						minlength: "Ваша тема повинна складатися не менше ніж з 4 символів."
+					},
+					number: {
+						required: "У Вас є номер, чи не так?",
+						minlength: "Ваш номер повинен складатися не менше ніж з 5 символів."
+					},
+					email: {
+						required: "Нема електронної пошти - нема повідомлення."
+					},
+					message: {
+						required: "Ем... так, Ви повинні написати щось, щоб відправити цю форму.",
+						minlength: "І це все? Дійсно?"
+					}
+				},
+				submitHandler: function (form) {
+					$(form).ajaxSubmit({
+						type: "POST",
+						data: $(form).serialize(),
+						url: "contact_process.php",
+						success: function () {
+							$('#contactForm :input').attr('disabled', 'disabled');
+							$('#contactForm').fadeTo("slow", 1, function () {
+								$(this).find(':input').attr('disabled', 'disabled');
+								$(this).find('label').css('cursor', 'default');
+								$('#success').fadeIn()
+								$('.modal').modal('hide');
+								$('#success').modal('show');
+							})
+						},
+						error: function () {
+							$('#contactForm').fadeTo("slow", 1, function () {
+								$('#error').fadeIn()
+								$('.modal').modal('hide');
+								$('#error').modal('show');
+							})
+						}
+					})
+				}
+			})
+		})
 
-    })(jQuery)
+	})(jQuery)
 })
